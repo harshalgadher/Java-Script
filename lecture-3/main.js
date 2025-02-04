4437b47a49315ec33d67d1c79d55c8ca452167f4
789harshal <harshalgadher04@gmail.com>
2025-02-04 15:09:40 +0530

    add lecture-3-4(project)

---
+++ lecture-3/main.js
@@ -0,0 +1,29 @@
+let num1 = prompt("Enter First number :");
+let num2 = prompt("Enter Second number :");
+
+//Arithmetic Operator
+console.log(parseInt(num1) + parseInt(num2));
+console.log(parseInt(num1) - parseInt(num2));
+console.log(parseInt(num1) * parseInt(num2));
+console.log(parseInt(num1) / parseInt(num2));
+console.log(parseInt(num1) % parseInt(num2));
+
+// comparison operator 
+console.log(num1 === num2);
+console.log(num1 == num2);
+console.log(num1 <= num2);
+console.log(num1 < num2);
+console.log(num1 > num2);
+console.log(num1 >= num2);
+console.log(num1 != num2);
+
+//Logical operators
+console.log(num1 || num2);
+console.log(num1 && num2);
+
+
+document.write(parseInt(num1) + parseInt(num2));
+document.write(parseInt(num1) - parseInt(num2));
+document.write(parseInt(num1) * parseInt(num2));
+document.write(parseInt(num1) / parseInt(num2));
+document.write(parseInt(num1) % parseInt(num2));
